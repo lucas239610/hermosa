02cc2f7b3672611e86c0bc2159005c76df7b6370
lucasdiaz <lucasssdiazz96@gmail.com>
2023-03-05 18:54:16 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let nombre = prompt("Ingrese su nombre y apellido con un espacio de por medio");
+
+function saludar(nombre) {
+
+    if (nombre === "victoria catuogno") {
+        alert(`Hola ${nombre}, la estabamos esperando... el creador de esta pagina le queria decir una cosita`);
+    } else {
+        alert(` ${nombre} escribi bien tu nombre !!! escriba su nombre y apellido sin abreviaturas`);
+}
+}
+
+saludar(nombre);
+
+        document.write( nombre + " TE AMO MUCHO !")
